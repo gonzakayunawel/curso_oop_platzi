c7344921c33f0709f935ba352d15876c214eb665
gonzakayunawel <go.cayunao@gmail.com>
2022-11-02 00:03:26 -0300

    Instancias y metodos const en Python

--- Python/main.py
+++ Python/main.py
@@ -1,18 +1,18 @@
 from car import Car
+from account import Account
 
 
 def run():
     print("Instanciando Objetos en Python...")
     
-    car = Car()
-    car.license = "AMS234"
-    car.driver = "Andres Herrera"
+    car = Car("AMS234", Account("Andres Herrera", "ANDA876"))
     print(vars(car))
+    print(vars(car.driver))
     
-    car2 = Car()
-    car2.license = "QWE567"
-    car2.driver = "Martha Erices"
-    print(vars(car2))
+    # car2 = Car()
+    # car2.license = "QWE567"
+    # car2.driver = "Martha Erices"
+    # print(vars(car2))
 
 if __name__ == "__main__":
     run()
