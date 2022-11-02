5cc9257e53dae02715849a5f93ba7f88ffb90a9f
gonzakayunawel <go.cayunao@gmail.com>
2022-11-01 22:27:05 -0300

    Definiendo clases en Python

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,18 @@
+from car import Car
+
 
 def run():
-    print("Hola Mundo")
+    print("Instanciando Objetos en Python...")
+    
+    car = Car()
+    car.license = "AMS234"
+    car.driver = "Andres Herrera"
+    print(vars(car))
+    
+    car2 = Car()
+    car2.license = "QWE567"
+    car2.driver = "Martha Erices"
+    print(vars(car2))
 
 if __name__ == "__main__":
     run()
