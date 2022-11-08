d7fdf7109c22ab2885280ab80fe555f492f28b87
gonzakayunawel <go.cayunao@gmail.com>
2022-11-08 00:35:50 -0400

    Reto Account en Python

--- Python/account.py
+++ Python/account.py
@@ -1,10 +1,17 @@
-class Account:
-    id=int
-    name=str
-    document=str
-    email=str
-    password=str
-
-    def __init__(self, name, document):
+class Account():
+    def __init__(self, id, name, email, password):
+        self.id = id
         self.name = name
-        self.document = document
+        self.email = email
+        self.password = password
+
+class User(Account):
+    def __init__(self, id, name, email, password, payment_method):
+        super().__init__(id, name, email, password)
+        self.payment_method = payment_method
+
+class Drive(Account):
+    def __init__(self, id, name, email, password, identif_doc, license):
+        super().__init__(id, name, email, password)
+        self.identif_doc = identif_doc
+        self.license = license
